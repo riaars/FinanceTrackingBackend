9e41147dffd23ce12fa1a4015f8219a0c9d825f4
Ria <riaratnasari@outlook.com>
2024-01-25 23:42:12 +0100

    add auth controllers

---
+++ controllers/auth.js
@@ -0,0 +1,52 @@
+const config = require('../config/auth')
+const db = require('../models')
+const User = db.user
+const Role = db.role
+
+var jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+
+exports.signup = (req, res) => {
+  const user = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: bcrypt.hashSync(req.body.password, 8),
+  })
+
+  user.save((err, user) => {
+    if (err) {
+      res.status(500).send({ message: err })
+      return
+    }
+    if (req.body.roles) {
+      Role.find(
+        {
+          name: { $in: req.body.roles },
+        },
+        (err, roles) => {
+          if (err) {
+            res.status(500).send({ message: err })
+            return
+          }
+          user.roles = roles.map((role) => role._id)
+          user.save((err) => {
+            if (err) {
+              res.status(500).send({ message: err })
+              return
+            }
+
+            res.send({ message: 'User was registered succesfully' })
+          })
+        },
+      )
+    } else {
+      Role.findOne({ name: 'user' }, (err, role) => {
+        if (err) {
+          res.status(500).send({ message: err })
+          return
+        }
+        res.send({ message: 'User was registered succesfully' })
+      })
+    }
+  })
+}
