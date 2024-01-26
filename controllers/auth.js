3572b9ceb747292d3442dac9f3edd07d8beb9173
Ria <riaratnasari@outlook.com>
2024-01-26 22:58:16 +0100

    add auth controllers sigin

--- controllers/auth.js
+++ controllers/auth.js
@@ -50,3 +50,50 @@ exports.signup = (req, res) => {
     }
   })
 }
+
+exports.signin = (req, res) => {
+  User.findOne({
+    email: req.body.email,
+  })
+    .poplate('roles', '-___v')
+    .exec((err, user) => {
+      if (err) {
+        res.status(500).send({ message: err })
+        return
+      }
+      if (!user) {
+        return res.status(404).send({ message: 'User not found' })
+      }
+
+      var passwordIsValid = bcrypt.compareSync(req.body.password, user.password)
+
+      if (!passwordIsValid) {
+        return res.status(401).send({
+          accessToken: null,
+          message: 'Invalid Password!',
+        })
+      }
+
+      const expirationTime = 86400
+      var dateNow = new Date()
+      var calculatedExpiresIn =
+        dateNow.getTime() +
+        expirationTime * 100 -
+        (dateNow.getTime() - dateNow.getMiliseconds()) / 1000
+      var token = jwt.sign({ id: user.id }, config.secret, {
+        expiresIn: calculatedExpiresIn,
+      })
+
+      var authorities = []
+      for (let i = 0; i < user.roles.length; i++) {
+        authorities.push(('ROLE_' = user.roles[i].name.toUpperCase()))
+      }
+      res.status(200).send({
+        id: user._id,
+        username: user.username,
+        email: user.email,
+        roles: authorities,
+        accessToken: token,
+      })
+    })
+}
